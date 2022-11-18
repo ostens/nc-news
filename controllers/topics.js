9c8bca5edb7674e1db00c27117b023478313e026
ostens <erinjayaustin@gmail.com>
2022-11-18 15:03:51 +0000

    22 - add POST /api/topics

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,9 +1,18 @@
-const { selectTopics } = require("../models/topics");
+const { selectTopics, insertTopic } = require("../models/topics");
 
 exports.getTopics = async (req, res, next) => {
   try {
     const topics = await selectTopics();
     res.status(200).send({ topics });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.postTopics = async (req, res, next) => {
+  try {
+    const topic = await insertTopic(req.body);
+    res.status(201).send({ topic });
   } catch (err) {
     next(err);
   }
--- errors/index.js
+++ errors/index.js
@@ -3,6 +3,8 @@ exports.psqlErrorHandler = (err, req, res, next) => {
     res.status(400).send({ msg: "Bad request" });
   } else if (err.code === "23503") {
     res.status(404).send({ msg: "Resource does not exist" });
+  } else if (err.code === "23505") {
+    res.status(400).send({ msg: "Resource already exists" });
   }
   next(err);
 };
