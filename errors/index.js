4905aa2703b98c26fef5a1abf3257f44d7171e01
ostens <erinjayaustin@gmail.com>
2022-11-18 14:17:05 +0000

    remove console log

--- errors/index.js
+++ errors/index.js
@@ -16,6 +16,5 @@ exports.customErrorHandler = (err, req, res, next) => {
 };
 
 exports.catchAllErrorHandler = (err, req, res, next) => {
-  console.log(err);
   res.status(500).send({ msg: "Internal server error" });
 };
